4dde87607be0de80ce6e9d7d2a086d04cb314e2b
Andrew Parks and Lois Choi <lois@>
2017-07-12 13:23:15 -0700

    add else if for removing first consonant letter

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var pigLatin = function(wordInput) {
   var vowelCaps = ["A", "E", "I", "O", "U"]
   var wordInputSplit = wordInput.split('');
   var firstLetter = wordInputSplit[0];
+  var consonant = ["c","d","f","g","h","i","j","k","l","m", "n", "p", "q", "r", "s", "t", "v", "x", "z", "y"];
 
   if (vowel.indexOf(firstLetter) != -1) {
     var firstLetterReturn = wordInputSplit.join('') + "way";
@@ -11,11 +12,12 @@ var pigLatin = function(wordInput) {
   else if(vowelCaps.indexOf(firstLetter) != -1) {
     var firstLetterReturn = wordInputSplit.join('') + "way";
     return firstLetterReturn;
-
+  }
+  else if(consonant.indexOf(firstLetter) != -1) {
+    var removeFirstCon = wordInputSplit.splice(0,1);
+    return removeFirstCon;
   }
   else {
-
-
     return wordInput;
   }
 };
