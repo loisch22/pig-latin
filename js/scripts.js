4944a77eb8e653d2b0f87369986fc758268b3b2b
Lois Choi <loisch22@gmail.com>
2017-07-12 22:17:03 -0700

    non-working js if statement to change first three letters of consonant and move to end of word + ay

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,18 @@ var pigLatin = function(wordInput) {
   var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
   var splittedWord = wordInput.split('');
   var twoLetters = [splittedWord[0], splittedWord[1]];
-  // var letters = [splittedWord[0], splittedWord[1], splittedWord[2]];
+  var threeLetters = [splittedWord[0], splittedWord[1], splittedWord[2]];
   var consonant = ["c","d","f","g","h","i","j","k","l","m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z", "C",
   "D","F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z", "Y"];
 
 
 
-   if ((consonant.indexOf(twoLetters[0]) != -1) && (consonant.indexOf(twoLetters[1]) != -1)) {
-     debugger;
-    var consTwo = splittedWord.slice(2); //why is the value not defined?
+   if ((consonant.indexOf(threeLetters[0]) != -1) && (consonant.indexOf(threeLetters[1]) != -1) && (consonant.indexOf(threeLetters[2]) != -1) {
+      var consThree = splittedWord.slice(3);
+      console.log(consThree);
+   }
+   else if ((consonant.indexOf(twoLetters[0]) != -1) && (consonant.indexOf(twoLetters[1]) != -1)) {
+    var consTwo = splittedWord.slice(2);
     consTwo.push(twoLetters.join(''));
     var newWordCopy2 = consTwo;
 
