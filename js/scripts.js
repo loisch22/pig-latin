5445cb1f06a800053cf40b54c44283ef288c29ff
Andrew Parks and Lois Choi <andrew+lois@>
2017-07-12 20:44:24 -0700

    add if statement to change first two letters of consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 var pigLatin = function(wordInput) {
   var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
   var splittedWord = wordInput.split('');
-  var twoLetters = splittedWord[0] + splittedWord[1];
+  var twoLetters = [splittedWord[0], splittedWord[1]];
   // var letters = [splittedWord[0], splittedWord[1], splittedWord[2]];
   var consonant = ["c","d","f","g","h","i","j","k","l","m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z", "C",
   "D","F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z", "Y"];
- debugger;
-   if (consonant.indexOf(twoLetters) != -1) {
-    var consTwo = splittedWord.slice(2);
-    consTwo.push(twoLetters);
+
+
+
+   if ((consonant.indexOf(twoLetters[0]) != -1) && (consonant.indexOf(twoLetters[1]) != -1)) {
+     debugger;
+    var consTwo = splittedWord.slice(2); //why is the value not defined?
+    consTwo.push(twoLetters.join(''));
     var newWordCopy2 = consTwo;
 
-    var joinedNewWord2 = newWordCopy.join('');
+    var joinedNewWord2 = newWordCopy2.join('');
 
     return joinedNewWord2 + "ay";
 
@@ -23,7 +26,6 @@ var pigLatin = function(wordInput) {
     var joinedNewWord = newWordCopy.join('');
 
     return joinedNewWord + "ay";
-
   } else if (vowels.indexOf(splittedWord[0]) != -1) {
     var vowelReturn = splittedWord.join('') + "way";
     return vowelReturn;
