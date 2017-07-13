0f638da7438bc81e9c749ff200f4f82bef5531ed
Andrew Parks and Lois Choi <andrew+lois@>
2017-07-12 19:15:01 -0700

    add twoLetters variable to js, add cap consonants, add cap vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 var pigLatin = function(wordInput) {
-  var vowel = ["a", "e", "i", "o", "u"];
-  var vowelCaps = ["A", "E", "I", "O", "U"]
+  var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
   var splittedWord = wordInput.split('');
-  var firstLetter = splittedWord[0];
-  var consonant = ["c","d","f","g","h","i","j","k","l","m", "n", "p", "q", "r", "s", "t", "v", "x", "z", "y"];
+  var twoLetters = splittedWord[0] + splittedWord[1];
+  // var letters = [splittedWord[0], splittedWord[1], splittedWord[2]];
+  var consonant = ["c","d","f","g","h","i","j","k","l","m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z", "C",
+  "D","F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z", "Y"];
+ debugger;
+   if (consonant.indexOf(twoLetters) != -1) {
+    var consTwo = splittedWord.slice(2);
+    consTwo.push(twoLetters);
+    var newWordCopy2 = consTwo;
 
-  if (vowel.indexOf(firstLetter) != -1) {
-    var firstLetterReturn = splittedWord.join('') + "way";
-    return firstLetterReturn;
-  }
-  else if(vowelCaps.indexOf(firstLetter) != -1) {
-    var firstLetterReturn = splittedWord.join('') + "way";
-    return firstLetterReturn;
-  }
-  else if(consonant.indexOf(firstLetter) != -1) {
+    var joinedNewWord2 = newWordCopy.join('');
+
+    return joinedNewWord2 + "ay";
+
+  } else if (consonant.indexOf(splittedWord[0]) != -1) {
     var sliced = splittedWord.slice(1);
     sliced.push(splittedWord[0]);
     var newWordCopy = sliced;
@@ -21,8 +23,11 @@ var pigLatin = function(wordInput) {
     var joinedNewWord = newWordCopy.join('');
 
     return joinedNewWord + "ay";
-  }
-  else {
+
+  } else if (vowels.indexOf(splittedWord[0]) != -1) {
+    var vowelReturn = splittedWord.join('') + "way";
+    return vowelReturn;
+  } else {
     return wordInput;
   }
 };
