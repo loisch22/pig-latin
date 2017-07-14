d94e814ba90ee18f3ab19f22e70c013c99455a58
Lois Choi <loisch22@gmail.com>
2017-07-13 19:38:38 -0700

    add new non-tested for loop to test if first letters are consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
-
-
+var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
 
 var pigLatin = function(wordInput) {
-
-  var vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
   var splittedWord = wordInput.split('');
-debugger;
+  var consWord = [];
+
   for (i = 0; i < vowels.length; i++) {
     if (splittedWord[0] === vowels[i]) {
-      return splittedWord.join('') + "way";
-    } else  if (splittedWord[0] != vowels[i]) {
-      var consWord1 = splittedWord.slice([1]);
-      consWord1.push(splittedWord[0]);
-      return consWord1.join('') + "ay";
-    } else if (splittedWord.indexOf(vowels) != vowels[i]) {
-      
+    return splittedWord.join('') + "way";
     }
-  }
-}
-    return wordInput;
-};
+  } for (j = 0; j < splittedWord.length; j++) {
+        while (splittedWord[j] != vowels[i]) {
+        var consWord1 = splittedWord[j];
+        consWord.push(consWord1);
+      } debugger;
+        var sliceWord = splittedWord.slice(vowels[i]);
+        sliceWord.push(consWord);
+
+        return sliceWord.join('') + "ay";
+      }
+    }
+
+    // return splittedWord.join('');
 
 
 $(document).ready(function() {
